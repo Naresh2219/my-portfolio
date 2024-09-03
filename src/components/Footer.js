3d39a5ef08ba59f60fd1afc42cdd6152845ceda2
Naresh Challa <cn818280@gmail.com>
2024-09-03 21:02:11 +0530

    title updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer style={{justifyContent:'flex-end'}}>
       <p>&copy; 2024 Naresh. All rights reserved.</p>
     </footer>
   );
